66ca280f10b7f71cf7a7d07ef6681ecfee58e565
ChamaraNilanga <chamaranilanga1999@gmail.com>
2024-03-15 09:33:12 +0530

    url change

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -23,128 +23,135 @@ import { useUserContext } from '../../ContextApi/UserContext';
 const defaultTheme = createTheme();
 
 export default function SignUp() {
+	const [message, setMessage] = useState('');
+	const [messageType, setMessageType] = useState('');
+	const { userName, setUserName } = useUserContext();
 
-  const [message, setMessage] = useState('');
-  const [messageType, setMessageType] = useState('');
-  const { userName , setUserName } = useUserContext();
+	const navigate = useNavigate();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log(data);
+		const emailUser = data.get('email');
+		const requestBody = {
+			email: data.get('email'),
+			password: data.get('password'),
+			name: data.get('firstName') + ' ' + data.get('lastName'),
+		};
+		try {
+			const response = await registerUser(requestBody);
+			if (response.status === 201 || response.status === 200) {
+				setMessage('User registered successfully');
+				setMessageType('success');
+				setUserName(emailUser);
+				setTimeout(() => {
+					setMessage('');
+					navigate('/login');
+				}, 3000);
+			}
+			console.log(response);
+		} catch (error) {
+			console.error(error);
+			setMessage('User registration failed');
+			setMessageType('error');
+		}
+	};
 
-  const navigate = useNavigate();
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log(data);
-    const emailUser = data.get('email');
-    const requestBody = {
-        email: data.get('email'),
-        password: data.get('password'),
-        firstName: data.get('firstName'),
-        lastName: data.get('lastName')
-    }
-    try {
-        const response = await registerUser(requestBody);
-        if(response.status === 201 || response.status === 200){
-            setMessage('User registered successfully');
-            setMessageType('success');
-            setUserName(emailUser);
-            setTimeout(() => {
-                setMessage('');
-                navigate('/login');
-            }, 3000);
-        }
-        console.log(response);
-    } catch (error) {
-        console.error(error);
-        setMessage('User registration failed');
-        setMessageType('error');
-    }
-    
-  };
-
-  return (
-    <ThemeProvider theme={defaultTheme}>
-        {message != "" && <Alert icon={<CheckCircleOutline fontSize="inherit" />} severity={messageType}>
-            {message}
-        </Alert>}
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			{message != '' && (
+				<Alert
+					icon={<CheckCircleOutline fontSize="inherit" />}
+					severity={messageType}
+				>
+					{message}
+				</Alert>
+			)}
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign up
+					</Typography>
+					<Box
+						component="form"
+						noValidate
+						onSubmit={handleSubmit}
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									autoComplete="given-name"
+									name="firstName"
+									required
+									fullWidth
+									id="firstName"
+									label="First Name"
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									required
+									fullWidth
+									id="lastName"
+									label="Last Name"
+									name="lastName"
+									autoComplete="family-name"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									autoComplete="email"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									autoComplete="new-password"
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign Up
+						</Button>
+						<Grid container justifyContent="flex-end">
+							<Grid item>
+								<Link href="/login" variant="body2">
+									Already have an account? Sign in
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 5 }} />
+			</Container>
+		</ThemeProvider>
+	);
 }
--- src/Service/axiosService.js
+++ src/Service/axiosService.js
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BaseUrl = "https://6185-2402-4000-2200-b65a-acb5-ad1a-b07d-8ce3.ngrok-free.app/api";
+const BaseUrl = 'https://8bc0-43-252-15-89.ngrok-free.app/api';
 
-const registerUser = (requestBody) => {        
-    return axios.post(`${BaseUrl}/register`, requestBody);
+const registerUser = (requestBody) => {
+	return axios.post(`${BaseUrl}/users/register`, requestBody);
 };
 
-const loginUser = (requestBody) => {        
-    return axios.post(`${BaseUrl}/login`, requestBody);
+const loginUser = (requestBody) => {
+	return axios.post(`${BaseUrl}/users/login`, requestBody);
 };
 
-const deletePost = (id) => {        
-    return axios.delete(`${BaseUrl}/admin/location/${id}`);
+const deletePost = (id) => {
+	return axios.delete(`${BaseUrl}/admin/location/${id}`);
 };
 
-export { registerUser , loginUser , deletePost };
+export { registerUser, loginUser, deletePost };
